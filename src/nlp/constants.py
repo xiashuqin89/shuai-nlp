543ca0c28ab1f17fd857a6fd65932bc140367b8b
neolei <neolei@tencent.com>
2022-06-22 21:13:27 +0800

    architecture component classifier 0.2

--- src/nlp/constants.py
+++ src/nlp/constants.py
@@ -1,13 +1,17 @@
 TEXT = 'text'
-INTENT = 'intent'
 TOKENS = 'tokens'
 TEXT_FEATURES = 'text_features'
+INTENT = 'intent'
+RANKING = 'intent_ranking'
 
 TOKENIZER_WHITESPACE = 'tokenizer_whitespace'
 TOKENIZER_JIEPA = 'tokenizer_jieba'
+
 FEATURIZER_REGEX = 'intent_entity_featurizer_regex'
 FEATURIZER_COUNT_VECTORS = 'intent_featurizer_count_vectors'
+
 CLASSIFIER_KEYWORD = 'intent_classifier_keyword'
+CLASSIFIER_SKLEARN = 'intent_classifier_sklearn'
 
 DEFAULT_DICT_FILE_NAME = 'jieba_default_dict'
 USER_DICTS_FOLDER_NAME = 'jieba_user_dicts/'
@@ -16,4 +20,3 @@ USER_DICT_FILE_NAME = USER_DICTS_FOLDER_NAME + 'user_dict.txt'
 REGEX_FEATURIZER_FILE_NAME = 'regex_featurizer.json'
 
 INTENT_RANKING_LENGTH = 10
-
