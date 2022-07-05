03cb5ac2f3f9e4b58945510168bbcc07ec807da3
neolei <neolei@tencent.com>
2022-07-05 15:56:19 +0800

    architecture shell cmd

--- cli/run.py
+++ cli/run.py
@@ -6,12 +6,13 @@ import six
 
 from src.nlp.components import ComponentBuilder
 from src.engine.runner.runner import Runner
+from src.common import logger
 from cli import Cmd
 
 
-class RunCmd(Cmd):
+class ShellCmd(Cmd):
     def __init__(self):
-        super().__init__()
+        super(ShellCmd, self).__init__()
 
     def create_argument_parser(self):
         parser = argparse.ArgumentParser(
@@ -19,26 +20,27 @@ class RunCmd(Cmd):
                         'for manual testing')
         parser.add_argument('-m', '--model', required=True,
                             help="path to model")
-        parser.add_argument('-t', '--text', required=True,
-                            help="path to model")
         return parser
 
     @classmethod
     def run(cls,
-            text: Text,
             model_path: Text,
             component_builder: ComponentBuilder = None) -> Text:
         interpreter = Runner.load(model_path, component_builder)
-        text = text.strip()
-        if six.PY2:
-            text = text.decode("utf-8")
-        r = interpreter.parse(text)
-        return json.dumps(r, indent=2)
+        logger.info("NLU model loaded. Type a message and "
+                    "press enter to parse it.")
+        while True:
+            text = input().strip()
+            if six.PY2:
+                text = text.decode("utf-8")
+            r = interpreter.parse(text)
+            print(json.dumps(r, indent=2))
+            logger.info("Next message:")
 
     def console(self) -> Text:
         cmdline_args = self.create_argument_parser()
-        return self.run(cmdline_args.text, cmdline_args.model)
+        return self.run(cmdline_args.model)
 
 
 if __name__ == '__main__':
-    RunCmd().console()
+    ShellCmd().console()
