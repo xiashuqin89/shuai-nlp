e408a82f235d9e36e6248cd9dd3a1efe00d56d57
neolei <neolei@tencent.com>
2022-07-15 17:37:00 +0800

    intent extractor debug 2.1

--- shuai/common/constants.py
+++ shuai/common/constants.py
@@ -1,9 +1,32 @@
-DEFAULT_CONFIG_LOCATION = "config.yml"
+DEFAULT_CONFIG_LOCATION = "default.yml"
 DEFAULT_CONFIG = {
+    'language': 'en',
+    'pipeline': [
+        {
+            'name': 'JiebaTokenizer',
+            'user_dicts': './corpus/user_dicts'
+        },
+        {
+            'name': 'TfIdfVectorsFeaturizer',
+            'stop_words': './corpus/stopwords.txt'
+        },
+        {
+            'name': 'TfIdfIntentClassifier'
+        },
+        {
+            'name': 'EntitySynonymMapper'
+        },
+        {
+            'name': 'RegexEntityExtractor'
+        }
+    ]
+}
+NULL_CONFIG = {
     "language": "en",
     "pipeline": [],
-    "data": None,
+    "data": None
 }
+
 # provide system default pipeline config
 # now config is not real config
 REGISTERED_PIPELINE_TEMPLATES = {
