c95724505b256b51041ce8ad22a20dd9a8168581
neolei <neolei@tencent.com>
2022-07-22 19:02:44 +0800

    intent extractor debug 3.0

--- shuai/__init__.py
+++ shuai/__init__.py
@@ -1,11 +1,11 @@
 from shuai.engine import Trainer, Runner
 from shuai.common import TrainerModelConfig, TrainingData, load_data_from_json
-from shuai.cli import Management, train, load, parse
+from shuai.cli import Management, train, load, parse, sign_regex
 
 
 __all__ = [
     "Runner", "Trainer",
     "TrainerModelConfig", "TrainingData", "load_data_from_json",
     "Management",
-    "train", "load", "parse"
+    "train", "load", "parse", "sign_regex"
 ]
