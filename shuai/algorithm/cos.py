32dbbc006e8e5980be14229a33a39f33c5a7324b
neolei <neolei@tencent.com>
2022-08-02 20:26:41 +0800

    intent extractor debug 4。0

--- shuai/algorithm/cos.py
+++ shuai/algorithm/cos.py
@@ -1,6 +1,8 @@
+from typing import List
+
 import numpy as np
 
 
-def cos(vec1: np.array, vec2: np.array) -> float:
+def cos(vec1: List, vec2: List) -> float:
     """cos similarity"""
     return float(np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2)))
